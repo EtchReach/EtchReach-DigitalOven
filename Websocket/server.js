557f20863d481c090dfc6180f82641ea942b49f5
Chay Hui Xiang <75465219+chayhuixiang@users.noreply.github.com>
2022-05-29 15:56:19 +0800

    Updated websocket backend and frontend

--- Websocket/server.js
+++ Websocket/server.js
@@ -1,6 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const appWs = require('express-ws')(app);
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json());
 
 app.ws('/echo', ws => {
     ws.on('message', msg => {
@@ -8,12 +18,15 @@ app.ws('/echo', ws => {
         ws.send(msg);
     });
     app.get('/',(req,res) => {
-        res.sendFile(path.join(__dirname,'/index.html'));
-    })
-    app.post('/light',(req,res) => {
-        ws.send('Hello World!');
+        res.json({status:'success',method:'get'})
+    });
+    app.post('/api',(req,res) => {
+        console.log('Got body:', req.body);
+        res.json({status:'success',method:'post', ...req.body});
     });
 });
+
+
 
 port = process.env.PORT || 5000;
 
